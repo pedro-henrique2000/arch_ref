c1e7bcb51370149bc22219732ca7d6e3e2a120ad
phb2000 <p.becker2000@hotmail.com>
2022-10-31 14:57:07 -0300

    first commit

---
+++ src/main/java/com/projects/arch_ref/ArchRefApplication.java
@@ -0,0 +1,13 @@
+package com.projects.arch_ref;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchRefApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArchRefApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/projects/arch_ref/ArchRefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.arch_ref;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchRefApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
