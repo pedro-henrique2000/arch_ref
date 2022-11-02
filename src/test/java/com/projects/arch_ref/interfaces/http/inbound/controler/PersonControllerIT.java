d9d7555b729246c199277ec4b66df5d4fa7d477f
phb2000 <p.becker2000@hotmail.com>
2022-11-02 16:02:39 -0300

    adding test container for IT tests

--- src/test/java/com/projects/arch_ref/interfaces/http/inbound/controler/PersonControllerIT.java
+++ src/test/java/com/projects/arch_ref/interfaces/http/inbound/controler/PersonControllerIT.java
@@ -6,13 +6,18 @@ import com.projects.arch_ref.infra.database.model.PersonModel;
 import com.projects.arch_ref.infra.database.repositories.jpa.JpaPersonRepository;
 import com.projects.arch_ref.interfaces.http.inbound.dto.GenderDTO;
 import com.projects.arch_ref.interfaces.http.inbound.dto.PersonRequest;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.time.LocalDate;
 import java.time.Month;
@@ -20,8 +25,11 @@ import java.time.Month;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@Testcontainers
+@ActiveProfiles("test")
 @SpringBootTest
 @AutoConfigureMockMvc
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class PersonControllerIT {
 
     @Autowired
@@ -33,9 +41,29 @@ public class PersonControllerIT {
     @Autowired
     private JpaPersonRepository jpaPersonRepository;
 
-    @BeforeEach()
+    @Container
+    private static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer("postgres:14.2-alpine");
+
+    @DynamicPropertySource
+    static void overrideTestProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+    }
+
+    @BeforeAll
+    static void setupContainer() {
+        postgreSQLContainer.start();
+    }
+
+    @AfterEach()
     void setup() {
         this.jpaPersonRepository.deleteAll();
+    }
+
+    @AfterAll
+    static void tearDown() {
+        postgreSQLContainer.stop();
     }
 
     @Test
@@ -73,7 +101,7 @@ public class PersonControllerIT {
         PersonRequest personRequest = new PersonRequest();
         personRequest.setEmail("pedro@mail.com");
         personRequest.setGender(GenderDTO.MALE);
-        personRequest.setBirthDate(LocalDate.MIN);
+        personRequest.setBirthDate(LocalDate.now().minusYears(18L));
         personRequest.setFirstName("pedro");
         personRequest.setLastName("h");
         return personRequest;
